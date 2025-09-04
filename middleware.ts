c57b61b38f58fb9f2d729ae0c93d47d545528419
behnam <adabzadehbehnam94>
2025-09-04 12:01:56 +0330

    fix bug middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
 
 import { NextResponse } from "next/server";
 
-export function middleware(request : {
-    url : string
-}){
-    return NextResponse.redirect(new URL("/login",request.url))
+export function middleware(request: {
+    url: string,
+    cookies: any,
+    get: string
+
+}) {
+    const cookie = request.cookies.get("name")
+
+    if (!cookie) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
 }
 
 export const config = {
-    matcher : '/dashboard'
+    matcher: '/dashboard'
 }
